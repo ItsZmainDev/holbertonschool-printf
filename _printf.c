12fdcbdcd92911f5b0ef6d05d63361bbe15a786f
ItsZmainDev <zmainpro@gmail.com>
2025-03-27 10:39:27 +0100

    Fix more 40 lines and compilation errors

--- _printf.c
+++ _printf.c
@@ -13,31 +13,39 @@ int _printf(const char *format, ...)
 	int length = 0;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
 
 	va_start(args, format);
 
 	while (*format)
-		if (*format == '%')
-			format++;
-
-			if (*format == 'c')
-				length += printf_char(va_arg(args, int));
-			else if (*format == 's')
-				length += printf_str(va_arg(args, char *));
-			else if (*format == '%')
-				length += _putchar('%');
-			else if (*format == 'd' || *format == 'i')
-				print_number(va_arg(args, int));
-			else
-				_putchar('%');
-				_putchar(*format);
-		else
-			length += _putchar(*format);
-
+	if (*format == '%')
+	{
 		format++;
+
+		if (*format == 'c')
+		{
+			length += printf_char(va_arg(args, int));
+		}
+		else if (*format == 's')
+		{
+			length += printf_str(va_arg(args, char *));
+		}
+		else if (*format == '%')
+		{
+			length += _putchar('%');
+		}
+		else if (*format == 'd' || *format == 'i')
+		{
+			print_number(va_arg(args, int));
+		}
+		else
+		{
+			_putchar('%');
+			_putchar(*format);
+		}
+	}
+
+	format++;
 
 	va_end(args);
 	return (length);
