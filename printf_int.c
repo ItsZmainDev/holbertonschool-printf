5ed27f93c78bc84c3f51c3bdf3517f2c2fc5423a
ItsZmainDev <zmainpro@gmail.com>
2025-03-27 14:21:34 +0100

    Fix betty code style

--- printf_int.c
+++ printf_int.c
@@ -7,29 +7,29 @@
 */
 void print_number(int n)
 {
-    char buffer[12];
-    int i = 0, temp = n;
+	char buffer[12];
+	int i = 0, temp = n;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-    if (n < 0)
-    {
-        _putchar('-');
-        temp = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
 
-    while (temp > 0)
-    {
-        buffer[i++] = (temp % 10) + '0';
-        temp /= 10;
-    }
+	while (temp > 0)
+	{
+		buffer[i++] = (temp % 10) + '0';
+		temp /= 10;
+	}
 
-    while (i--)
-    {
-        _putchar(buffer[i]);
-    }
+	while (i--)
+	{
+		_putchar(buffer[i]);
+	}
 }
