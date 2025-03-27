1eec8f105c678b2e8fd184c6a32b133808336305
ItsZmainDev <zmainpro@gmail.com>
2025-03-27 10:28:49 +0100

    Fix more 40 line in _printf function

--- _printf.c
+++ _printf.c
@@ -20,40 +20,24 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 
 	while (*format)
-	{
 		if (*format == '%')
-		{
 			format++;
 
 			if (*format == 'c')
-			{
-				length += _putchar(va_arg(args, int));
-			}
+				length += printf_char(va_arg(args, int));
 			else if (*format == 's')
-			{
 				length += printf_str(va_arg(args, char *));
-			}
 			else if (*format == '%')
-			{
 				length += _putchar('%');
-			}
 			else if (*format == 'd' || *format == 'i')
-			{
 				print_number(va_arg(args, int));
-			}
 			else
-			{
 				_putchar('%');
 				_putchar(*format);
-			}
-		}
 		else
-		{
 			length += _putchar(*format);
-		}
 
 		format++;
-	}
 
 	va_end(args);
 	return (length);
